a3a84a8ccd6b0738ec133c479092dfd0a28f21a6
sai9100663540 <97456227+sai9100663540@users.noreply.github.com>
2022-02-09 13:59:58 +0530

    reverse

---
+++ src/main/java/Reversenumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Reversenumber {
+    public static void main(String[] args) {
+        System.out .println("Enter number: ");
+        Scanner sc=new Scanner(System.in);
+        int num = sc.nextInt();
+        int rev =0;
+        int rem;
+        while( num != 0){
+            rem = num%10;
+            rev = rev*10 + rem;
+            num = num/10 ;
+        }
+        System.out.println(rev);
+    }
+}
