5a319332c2bddfc7eaf14716aff78b3967333543
sai9100663540 <97456227+sai9100663540@users.noreply.github.com>
2022-02-09 13:48:58 +0530

    divison

---
+++ src/main/java/quotientRemainder.java
@@ -0,0 +1,11 @@
+public class quotientRemainder {
+    public static void main(String[] args) {
+        int dividend = 19, divisor = 6;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
