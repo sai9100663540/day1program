96123e39b9ee97adcbd9bc95b3ccf914feea5e9e
sai9100663540 <97456227+sai9100663540@users.noreply.github.com>
2022-02-09 11:39:43 +0530

    largestnumber

---
+++ src/main/java/largestnumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class largestnumber {
+    public static void main(String[] args) {
+        System.out.print("a : ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        System.out.print("b : ");
+        int b = sc.nextInt();
+
+        System.out.print("c : ");
+        int c = sc.nextInt();
+
+        if (a>b && a>c)
+            System.out.println("Largest number is:" + a);
+        else if (b>c)
+            System.out.println("Largest number is:" + b);
+        else
+            System.out.println("Largest number is:" + c);
+
+
+    }
+}
