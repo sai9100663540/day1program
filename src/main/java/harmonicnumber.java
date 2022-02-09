a1d2879797da398cbf0d9f962efbdd5dd374fa33
sai9100663540 <97456227+sai9100663540@users.noreply.github.com>
2022-02-09 11:45:32 +0530

    harmonic

---
+++ src/main/java/harmonicnumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class harmonicnumber {
+    public static void main(String[] args) {
+        System.out.print("enter your value here :");
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        double harmonicSum = 0.0;
+        for (int i = 1; i <= count; i++) {
+            if (i < count)
+                System.out.print("1/" + i + " + ");
+            else
+                System.out.print("1/" + i);
+            harmonicSum += 1f / i;
+        }
+        System.out.println();
+        System.out.println(harmonicSum);
+
+    }
+}
