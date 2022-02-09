f838a55bc91b591e1fa6d276512c42762e2cc3f1
sai9100663540 <97456227+sai9100663540@users.noreply.github.com>
2022-02-09 13:53:10 +0530

    even add

---
+++ src/main/java/evenodd.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class evenodd {
+    public static void main(String[] args) {
+        System.out.println("give a number : ");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if (x%2 == 0)
+            System.out.println("Even number");
+        else
+            System.out.println("Odd number");
+    }
+}
